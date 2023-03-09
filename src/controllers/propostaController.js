a35055e2fd9212ee722361f30958e758774c1773
Rodrigo Onesko Dias <rodrigo_onesko@hotmail.com>
2023-03-09 14:31:00 -0300

    Update propostaController.js

--- src/controllers/propostaController.js
+++ src/controllers/propostaController.js
@@ -184,3 +184,68 @@ module.exports = {
         }
     },
 }
+
+
+function ExcelDateToJSDate(serial) {
+    var utc_days = Math.floor(serial - 25569);
+    var utc_value = utc_days * 86400;
+    var date_info = new Date(utc_value * 1000);
+
+    var fractional_day = serial - Math.floor(serial) + 0.0000001;
+
+    var total_seconds = Math.floor(86400 * fractional_day);
+
+    var seconds = total_seconds % 60;
+
+    total_seconds -= seconds;
+
+    var hours = Math.floor(total_seconds / (60 * 60));
+    var minutes = Math.floor(total_seconds / 60) % 60;
+
+    return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
+}
+
+function calcularIdade(data) {
+    var now = new Date();
+    var today = new Date(now.getYear(), now.getMonth(), now.getDate());
+
+    var yearNow = now.getYear();
+    var monthNow = now.getMonth();
+    var dateNow = now.getDate();
+    var dob = new Date(data.substring(6, 10),
+        data.substring(3, 5) - 1,
+        data.substring(0, 2)
+    );
+
+    var yearDob = dob.getYear();
+    var monthDob = dob.getMonth();
+    var dateDob = dob.getDate();
+    var age = {};
+    yearAge = yearNow - yearDob;
+
+    if (monthNow >= monthDob)
+        var monthAge = monthNow - monthDob;
+    else {
+        yearAge--;
+        var monthAge = 12 + monthNow - monthDob;
+    }
+
+    if (dateNow >= dateDob)
+        var dateAge = dateNow - dateDob;
+    else {
+        monthAge--;
+        var dateAge = 31 + dateNow - dateDob;
+
+        if (monthAge < 0) {
+            monthAge = 11;
+            yearAge--;
+        }
+    }
+
+    age = {
+        years: yearAge,
+        months: monthAge,
+        days: dateAge
+    };
+    return age.years;
+}
