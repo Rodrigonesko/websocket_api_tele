2155070fd97962cb4a4d5dd624db8b0dddf721ce
Rodrigo Onesko Dias <rodrigo_onesko@hotmail.com>
2023-04-19 11:19:13 -0300

    Update lembreteMensagem.js

--- src/tasks/lembreteMensagem.js
+++ src/tasks/lembreteMensagem.js
@@ -2,6 +2,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const TwilioNumber = process.env.TWILIO_NUMBER
+const moment = require('moment')
 
 const PropostaEntrevista = require('../models/PropostaEntrevista')
 const Chat = require('../models/Chat')
