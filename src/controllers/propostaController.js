8202c9086ccae37d482f428d7fa9c0f369a0273b
Rodrigo Onesko Dias <rodrigo_onesko@hotmail.com>
2023-12-08 09:18:01 -0300

    adicionando tratamento de erro no upload de planilha

--- src/controllers/propostaController.js
+++ src/controllers/propostaController.js
@@ -60,212 +60,238 @@ module.exports = {
                 }
                 acc.push({ ...item });
                 return acc;
-
             }, [])
 
             let counter = 0;
             const wppSenders = [TwilioNumber, TwilioNumberPme, TwilioNumberSP];
 
 
+
             for (const item of arrCpfTitulares) {
 
-                let wppSender = wppSenders[counter % wppSenders.length];
-                counter++;
+                try {
+                    let wppSender = wppSenders[counter % wppSenders.length];
+                    counter++;
 
-                let proposta = item.NUM_PROPOSTA
+                    let proposta = item.NUM_PROPOSTA
 
-                //verifica se a proposta é um número
+                    //verifica se a proposta é um número
 
-                if (typeof (proposta) !== 'number') {
-                    proposta = proposta.replace(/\D/g, '')
+                    if (typeof (proposta) !== 'number') {
+                        proposta = proposta.replace(/\D/g, '')
+                    }
+
+                    // const proposta = item?.NUM_PROPOSTA?.replace(/\D/g, '');
+
+                    let vigencia = ExcelDateToJSDate(item.DT_VENDA)
+                    vigencia.setDate(vigencia.getDate() + 1)
+                    vigencia = moment(vigencia).format('YYYY-MM-DD')
+
+                    const filial = item.FILIAL
+
+                    const riscoBeneficiario = item.RISCO_BENEF
+
+                    const riscoImc = item.RISCO_IMC
+
+                    const sinistral = item.SINISTRALIDADE_ANT
+
+                    const liminar = item.LIMINAR_ANT
+
+                    const fraude = item.FRAUDE_ANT
+
+                    const propCancel = item.PROP_CANCEL_UNDER_ANT
+
+                    const sinistContr = item.SINIST_CONTR_ANT
+
+                    const corretora = item.CORRETORA
+
+                    const corretor = item.CORRETOR
+
+                    let cpf = ''
+
+                    console.log(item.NUM_CPF);
+
+                    //Verificar se o item.NUM_CPF não é um número
+
+                    if (typeof (item.NUM_CPF) !== 'number' && item.NUM_CPF !== undefined) {
+                        cpf = item.NUM_CPF.replace(/\D/g, '')
+                    } else {
+                        cpf = item.NUM_CPF
+                    }
+
+                    const nome = item.NOME_ASSOCIADO
+
+                    const sexo = item.SEXO
+
+                    const tipoAssociado = item.TIPO_ASSOCIADO
+
+                    const tipoContrato = item.TIPO_CONTRATO
+
+                    const nomeOperadora = item.NOM_OPERADORA
+
+                    vigencia = moment().businessAdd(2).format('YYYY-MM-DD')
+
+                    const grupoCarencia = item.GRUPO_CARENCIA
+
+                    const d1 = item.DS_1
+                    const d2 = item.DS_2
+                    const d3 = item.DS_3
+                    const d4 = item.DS_4
+                    const d5 = item.DS_5
+                    const d6 = item.DS_6
+                    const d7 = item.DS_7
+                    const d8 = item.DS_8
+                    const d9 = item.DS_9
+
+                    const peso = item.PESO
+                    const altura = item.ALTURA
+                    const imc = item.IMC
+
+                    const cid1 = item.CID_PI_ANT_1
+                    const cid2 = item.CID_PI_ANT_2
+                    const cid3 = item.CID_PI_ANT_3
+
+                    // let wppSender = TwilioNumber
+
+                    // if (tipoContrato.toLowerCase().indexOf('pme') !== -1) {
+                    //     wppSender = TwilioNumberPme
+                    // }
+
+                    const observacao = item['OBSERVAÇÕES']
+                    const ddd = item.NUM_DDD_CEL || item.NUM_DDD_TEL
+                    let numero = item.NUM_CEL?.toString().replace(/\s/g, '') || item.NUM_TEL?.toString().replace(/\s/g, '')
+
+                    let telefone = ''
+
+                    if (ddd && numero) {
+                        telefone = `(${ddd}) ${numero}`
+                    }
+
+                    // if (ddd == '11') {
+                    //     wppSender = TwilioNumberSP
+                    // }
+
+                    if (numero?.length !== 9 && numero !== undefined) {
+                        numero = `9${numero}`
+                        console.log(numero);
+                    }
+
+                    const celularCompleto = `55${ddd}${numero}`
+
+                    let whatsapp = `whatsapp:+55`
+
+                    if (ddd && numero) {
+                        whatsapp = `whatsapp:+55${ddd}${numero}`
+                    }
+                    whatsapp = whatsapp.replace(/\s/g, '')
+                    let dataNascimento
+
+                    if (typeof (item.DT_NASC) === 'number') {
+                        dataNascimento = ExcelDateToJSDate(item.DT_NASC)
+                        dataNascimento.setDate(dataNascimento.getDate() + 1)
+                        dataNascimento = moment(dataNascimento).format('DD/MM/YYYY')
+                        idade = calcularIdade(dataNascimento)
+                    } else {
+                        dataNascimento = item.DT_NASC
+                        dataNascimento = dataNascimento.replace(' ', '')
+                        idade = calcularIdade(dataNascimento)
+                    }
+
+                    let formulario
+
+                    if (idade <= 2) {
+                        formulario = '0-2 anos'
+                    }
+
+                    if (idade >= 3 && idade <= 8) {
+                        formulario = '2-8 anos'
+                    }
+
+                    if (idade >= 9) {
+                        formulario = 'adulto'
+                    }
+
+                    const cpfTitular = item.cpfTitular
+
+                    let situacao = 'A enviar'
+
+                    if (!cpfTitular) {
+                        situacao = 'Corrigir'
+                    }
+                    const resultado = {
+                        dataRecebimento: moment().format('YYYY-MM-DD'),
+                        proposta,
+                        filial,
+                        riscoBeneficiario,
+                        riscoImc,
+                        sinistral,
+                        liminar,
+                        fraude,
+                        propCancel,
+                        sinistContr,
+                        corretora,
+                        corretor,
+                        cpf,
+                        nome,
+                        sexo,
+                        dataNascimento,
+                        idade,
+                        d1,
+                        d2,
+                        d3,
+                        d4,
+                        d5,
+                        d6,
+                        d7,
+                        d8,
+                        d9,
+                        observacao,
+                        vigencia,
+                        telefone,
+                        grupoCarencia,
+                        peso,
+                        altura,
+                        imc,
+                        cid1,
+                        cid2,
+                        cid3,
+                        tipoAssociado,
+                        tipoContrato,
+                        formulario,
+                        situacao,
+                        cpfTitular,
+                        ddd,
+                        celular: numero,
+                        whatsapp,
+                        celularCompleto,
+                        nomeOperadora,
+                        wppSender,
+                        newStatus: 'Agendar',
+                    }
+
+                    const existeProposta = await PropostaEntrevista.findOne({
+                        proposta,
+                        nome
+                    })
+
+                    if (!existeProposta) {
+                        const newPropostaEntrevista = await PropostaEntrevista.create(resultado)
+                        quantidade++
+                    }
+                } catch (error) {
+                    console.log(error);
+                    return res.status(400).json({
+                        error: `Erro ao processar a proposta ${item.NUM_PROPOSTA}, Todas as propostas anteriores foram inseridas`
+                    })
+                    //throw new Error(`Erro ao processar a proposta ${item.NUM_PROPOSTA}`)
                 }
-
-                // const proposta = item?.NUM_PROPOSTA?.replace(/\D/g, '');
-
-                let vigencia = ExcelDateToJSDate(item.DT_VENDA)
-                vigencia.setDate(vigencia.getDate() + 1)
-                vigencia = moment(vigencia).format('YYYY-MM-DD')
-
-                const filial = item.FILIAL
-
-                const riscoBeneficiario = item.RISCO_BENEF
-
-                const riscoImc = item.RISCO_IMC
-
-                const sinistral = item.SINISTRALIDADE_ANT
-
-                const liminar = item.LIMINAR_ANT
-
-                const fraude = item.FRAUDE_ANT
-
-                const propCancel = item.PROP_CANCEL_UNDER_ANT
-
-                const sinistContr = item.SINIST_CONTR_ANT
-
-                const corretora = item.CORRETORA
-
-                const corretor = item.CORRETOR
-
-                const cpf = item.NUM_CPF
-
-                const nome = item.NOME_ASSOCIADO
-
-                const sexo = item.SEXO
-
-                const tipoAssociado = item.TIPO_ASSOCIADO
-
-                const tipoContrato = item.TIPO_CONTRATO
-
-                const nomeOperadora = item.NOM_OPERADORA
-
-                vigencia = moment().businessAdd(2).format('YYYY-MM-DD')
-
-                const grupoCarencia = item.GRUPO_CARENCIA
-
-                const d1 = item.DS_1
-                const d2 = item.DS_2
-                const d3 = item.DS_3
-                const d4 = item.DS_4
-                const d5 = item.DS_5
-                const d6 = item.DS_6
-                const d7 = item.DS_7
-                const d8 = item.DS_8
-                const d9 = item.DS_9
-
-                const peso = item.PESO
-                const altura = item.ALTURA
-                const imc = item.IMC
-
-                const cid1 = item.CID_PI_ANT_1
-                const cid2 = item.CID_PI_ANT_2
-                const cid3 = item.CID_PI_ANT_3
-
-                // let wppSender = TwilioNumber
-
-                // if (tipoContrato.toLowerCase().indexOf('pme') !== -1) {
-                //     wppSender = TwilioNumberPme
-                // }
-
-                const observacao = item['OBSERVAÇÕES']
-                const ddd = item.NUM_DDD_CEL || item.NUM_DDD_TEL
-                let numero = item.NUM_CEL?.toString().replace(/\s/g, '') || item.NUM_TEL?.toString().replace(/\s/g, '')
-                const telefone = `(${ddd}) ${numero}`
-
-                // if (ddd == '11') {
-                //     wppSender = TwilioNumberSP
-                // }
-
-                if (numero?.length !== 9 && numero !== undefined) {
-                    numero = `9${numero}`
-                    console.log(numero);
-                }
-
-                const celularCompleto = `55${ddd}${numero}`
-                let whatsapp = `whatsapp:+55${ddd}${numero}`
-                whatsapp = whatsapp.replace(/\s/g, '')
-
-                let dataNascimento
-
-                if (typeof (item.DT_NASC) === 'number') {
-                    dataNascimento = ExcelDateToJSDate(item.DT_NASC)
-                    dataNascimento.setDate(dataNascimento.getDate() + 1)
-                    dataNascimento = moment(dataNascimento).format('DD/MM/YYYY')
-                    idade = calcularIdade(dataNascimento)
-                } else {
-                    dataNascimento = item.DT_NASC
-                    dataNascimento = dataNascimento.replace(' ', '')
-                    idade = calcularIdade(dataNascimento)
-                }
-
-                let formulario
-
-                if (idade <= 2) {
-                    formulario = '0-2 anos'
-                }
-
-                if (idade >= 3 && idade <= 8) {
-                    formulario = '2-8 anos'
-                }
-
-                if (idade >= 9) {
-                    formulario = 'adulto'
-                }
-
-                const cpfTitular = item.cpfTitular
-
-                let situacao = 'A enviar'
-
-                if (!cpfTitular) {
-                    situacao = 'Corrigir'
-                }
-                const resultado = {
-                    dataRecebimento: moment().format('YYYY-MM-DD'),
-                    proposta,
-                    filial,
-                    riscoBeneficiario,
-                    riscoImc,
-                    sinistral,
-                    liminar,
-                    fraude,
-                    propCancel,
-                    sinistContr,
-                    corretora,
-                    corretor,
-                    cpf,
-                    nome,
-                    sexo,
-                    dataNascimento,
-                    idade,
-                    d1,
-                    d2,
-                    d3,
-                    d4,
-                    d5,
-                    d6,
-                    d7,
-                    d8,
-                    d9,
-                    observacao,
-                    vigencia,
-                    telefone,
-                    grupoCarencia,
-                    peso,
-                    altura,
-                    imc,
-                    cid1,
-                    cid2,
-                    cid3,
-                    tipoAssociado,
-                    tipoContrato,
-                    formulario,
-                    situacao,
-                    cpfTitular,
-                    ddd,
-                    celular: numero,
-                    whatsapp,
-                    celularCompleto,
-                    nomeOperadora,
-                    wppSender,
-                    newStatus: 'Agendar',
-                }
-
-                const existeProposta = await PropostaEntrevista.findOne({
-                    proposta,
-                    nome
-                })
-
-                if (!existeProposta) {
-                    const newPropostaEntrevista = await PropostaEntrevista.create(resultado)
-                    quantidade++
-                }
-
             }
 
             return res.status(200).json({ message: `Foram inseridas ${quantidade} novas propostas!` })
         } catch (error) {
             console.log(error);
             return res.status(500).json({
-                error: "Internal server error."
+                error: "Internal server error.",
             })
         }
     },
