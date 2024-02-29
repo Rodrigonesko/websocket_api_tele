99259168abb6883ed108e4a18dd0ff6d84071c00
Rodrigo Onesko Dias <rodrigo_onesko@hotmai.com>
2024-02-29 10:14:56 -0300

    corrigindo token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,7 +3,15 @@ const secret = process.env.JWT_SECRET
 
 const auth = (req, res, next) => {
     try {
-        const token = req.headers['authorization'].split(' ')[1]
+        let token = req.cookies['token'] ? req.cookies['token'] : ""
+
+        if (!token) {
+            if (!req.headers['authorization']) {
+                return res.status(401).json({ message: "No authorization header present" })
+            }
+            token = req.headers['authorization'].split(' ')[1]
+        }
+
         const data = jwt.verify(token, secret)
         req.user = data.username
         req.email = data.email
